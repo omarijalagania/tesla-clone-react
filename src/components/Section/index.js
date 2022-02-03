64da085f0ce0e03eb6f6b0775d70a468c5de84e0
omari <omari.jalagania@gmail.com>
2022-02-03 22:33:48 +0400

    first comment

---
+++ src/components/Section/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+function Section() {
+  return (
+    <Wrap>
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+      <ButtonGroup>
+        <LeftButton>Custom Order</LeftButton>
+        <RightButton>Existing Inventory</RightButton>
+      </ButtonGroup>
+    </Wrap>
+  );
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/model-s.jpg");
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div``;
+
+const LeftButton = styled.div``;
+
+const RightButton = styled(LeftButton)``;
